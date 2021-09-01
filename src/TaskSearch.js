0427ff03de443f88c5a0a0d365336b14c471985d
Luis Alejandro <luisalejandronietoruth@gmail.com>
2021-09-01 09:52:25 -0500

    Using the State Hook

--- src/TaskSearch.js
+++ src/TaskSearch.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './TaskSearch.css'
 
 function TaskSearch() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
   const onChangeValueSearch = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   }
-  return (
+  return [
     <input
       className="TaskSearch" placeholder="Buscar"
       onChange={onChangeValueSearch}
-
-      />
-  );
+      value={searchValue}
+      />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TaskSearch };
